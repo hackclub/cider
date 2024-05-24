171f2327f04ebb03bdc54610bf593124f98f7c72
Faisal Sayed <faisal.sayed502@gmail.com>
2024-05-24 14:42:11 -0400

    add desc to projects

--- website/components/Project.js
+++ website/components/Project.js
@@ -1,7 +1,7 @@
-export default function Testimonial({ image, name, author, link }) {
+export default function Testimonial({ image, name, author, link, description }) {
   return (
     <a href={link} className="hover:scale-105 transition-all" target="_blank">
-      <div className="flex flex-col items-left gap-4 border border-gray-400 lg:w-64 w-full rounded-xl p-8">
+      <div className="flex flex-col items-left justify-between gap-4 border border-gray-400 lg:w-64 w-full rounded-xl p-8 lg:h-full">
         {/* {image && (
         <img
           src={image}
@@ -11,8 +11,9 @@ export default function Testimonial({ image, name, author, link }) {
         <p className="text-xl font-medium text-gray-700">
           {name}
           <br />
-          <span className="text-lg font-semibold text-red">-&nbsp;{author}</span>
+          <span className="text-base text-gray-500">{description}</span>
         </p>
+        <p className="text-lg font-semibold text-red">-&nbsp;{author}</p>
       </div>
     </a>
   );
