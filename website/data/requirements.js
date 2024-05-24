d9c044d37ae2fd8aaff1bd335348314b14ca5520
Faisal Sayed <faisal.sayed502@gmail.com>
2024-05-24 13:01:36 -0400

    fix requirements

--- website/data/requirements.js
+++ website/data/requirements.js
@@ -1,10 +1,8 @@
 export const requirements = [
-  "Your app must be original & unique",
-  "It must be built using either React Native or SwiftUI",
-  "It must be open-source",
+  "Your app must be open-source, original, unique & built using either React Native or SwiftUI",
+  "It is recommended to use #hack-hour to log your progress while building the app.",
   "Your app must have 25 users using your app while it's in TestFlight.",
   "The app should solve a real problem or add significant value to the users' lives. Describe how the app will impact its users and what specific issues it addresses.",
-  "It is recommended to use #hack-hour to log your progress while building the app.",
   "The app should have at least five unique screens (e.g., home, profile, settings, notifications, and a feature-specific screen).",
   "The app must include at least three core features that work together to create a cohesive experience. For example, a social network app could include user profiles, a news feed, and direct messaging.",
   "The app must have some sort of data management system, using either local storage, cloud databases, or APIs.",
