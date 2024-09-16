f03db5a431c350fe9feb13390f4399b62c429b2a
Faisal Sayed <faisal.sayed502@gmail.com>
2024-09-16 13:40:10 -0400

    feat: remove 25 users requirement

--- website/data/requirements.js
+++ website/data/requirements.js
@@ -1,7 +1,6 @@
 export const requirements = [
   "Your app must be open-source, original, unique & built using either React Native or SwiftUI",
   "It is recommended to use #arcade to log your progress while building the app.",
-  "Your app must have 25 users using your app while it's in TestFlight.",
   "The app should solve a real problem or add significant value to the users' lives. Describe how the app will impact its users and what specific issues it addresses.",
   "The app should have at least five unique screens (e.g., home, profile, settings, notifications, and a feature-specific screen).",
   "The app must include at least three core features that work together to create a cohesive experience. For example, a social network app could include user profiles, a news feed, and direct messaging.",
